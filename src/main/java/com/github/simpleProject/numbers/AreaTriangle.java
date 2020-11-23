224aa905d93bd2706761e7eaaf26d7e90d5c39f9
SITARAM <sitaram.garimella@outlook.com>
2020-11-23 11:26:35 +0100

    Updated Junit

--- src/main/java/com/github/simpleProject/numbers/AreaTriangle.java
+++ src/main/java/com/github/simpleProject/numbers/AreaTriangle.java
@@ -1,19 +1,27 @@
 package com.github.simpleProject.numbers;
 
 public class AreaTriangle {
-	/**
-	 * area of a triagle
-	 * 
-	 * @param base   base in cm
-	 * @param height height in cm
-	 * @return area of triangle
-	 */
-	public static double areaTriangle(double base, double height) {
 
-		if (base < 0 || height < 0 ) {
+	/**
+	 * Calculate Area of triangle
+	 * 
+	 * @param a side of a triangle
+	 * @param b side of a triangle
+	 * @param c side of a triangle
+	 * @return Area of a triangle
+	 */
+	public static double areaTriangle(double a, double b, double c) {
+		if (a < 0 || b < 0 || c < 0) {
 			throw new IllegalArgumentException("Negative values not allowed!");
 		}
-		return 0.5 * base * height;
+
+		if ((a + b < c) || (b + c < a) || (c + a < b)) {
+			throw new IllegalArgumentException("One Side can't be greater than other two !");
+		}
+
+		double s = (a + b + c) / 2;
+
+		return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 
 	}
 }
--- src/test/java/com/github/simpleProject/numbers/AreaTriangleTest.java
+++ src/test/java/com/github/simpleProject/numbers/AreaTriangleTest.java
@@ -9,13 +9,21 @@ public class AreaTriangleTest {
 
 	@Test
 	public void area_triangle_test() {
-		assertEquals(5, AreaTriangle.areaTriangle(2, 5));
+		assertEquals(127.31236192923294, AreaTriangle.areaTriangle(14, 56, 43));
 	}
 
 	@Test
 	public void are_triangle_negative_numbers_test() {
 		assertThrows(IllegalArgumentException.class, () -> {
-			AreaTriangle.areaTriangle(-2, 5);
+			AreaTriangle.areaTriangle(-2, 5,1);
+		});
+
+	}
+	
+	@Test
+	public void are_triangle_greater_side_test() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			AreaTriangle.areaTriangle(2, 5,1);
 		});
 
 	}
